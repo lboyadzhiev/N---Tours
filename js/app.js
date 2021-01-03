cc7daf1231da58b82404798259ff3c02acf883c9
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-01-03 12:08:51 +0200

    fixing some typos

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 window.addEventListener('load', async () => {
-   const rootEl = document.querySelector('#root');
+   const rootEl = document.querySelector('#home');
+   const mainEl = document.querySelector('#main');
 
    const headerStr = await (await fetch('../Templates/header.hbs')).text();
    const headerTempl = Handlebars.compile(headerStr);
 
-   const headerHtml = headerTempl({ });
+   const aboutSection = await (await (await fetch('../Templates/about.hbs')).text());
+   const aboutTempl = Handlebars.compile(aboutSection);
+
+   const headerHtml = headerTempl({});
+   const aboutHtml = aboutTempl({});
 
    rootEl.innerHTML = headerHtml;
+   mainEl.innerHTML = aboutHtml;
 });
