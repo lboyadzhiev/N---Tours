c368c6dedfade1458e1b79854e53b4a6033b20d1
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-01-06 17:06:23 +0200

    delete Templates

--- js/app.js
+++ js/app.js
@@ -1,21 +1,3 @@
 window.addEventListener('load', async () => {
-   const rootEl = document.querySelector('#home');
-   const mainEl = document.querySelector('#main');
 
-   const headerStr = await (await fetch('../Templates/header.hbs')).text();
-   const headerTempl = Handlebars.compile(headerStr);
-
-   const aboutSection = await (await (await fetch('../Templates/about.hbs')).text());
-   const aboutTempl = Handlebars.compile(aboutSection);
-
-   const featuresSection = await (await (await fetch('../Templates/features.hbs')).text());
-   const featuresTempl = Handlebars.compile(featuresSection);
-
-   const headerHtml = headerTempl({});
-   const aboutHtml = aboutTempl({});
-   const featuresHtml = featuresTempl({});
-   const main = aboutHtml + featuresHtml;
-
-   rootEl.innerHTML = headerHtml;
-   mainEl.innerHTML = main;
 });
